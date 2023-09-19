b371ae7fca073585242686dc308af90666976475
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-09-19 13:20:23 +0700

    Created HouseCreate.js added html

---
+++ src/pages/HouseCreate.js
@@ -0,0 +1,115 @@
+function HouseCreate() {
+  return (
+    <>
+      <div className="header">
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <img
+              className="logo"
+              style={{ width: '7rem', height: 'auto' }}
+              src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
+            />
+            <form className="d-flex" role="search">
+              <button
+                className="btn btn-outline-success login-button me-2"
+                type="submit"
+              >
+                <img
+                  className="profile-pic rounded-circle me-1"
+                  style={{ width: '1.27rem', height: '1.27rem' }}
+                  src="images/IMG_8468.jpg"
+                />
+                Daelum M
+              </button>
+              <button
+                className="btn btn-outline-success logout-button"
+                type="submit"
+              >
+                Logout
+              </button>
+            </form>
+          </div>
+        </nav>
+      </div>
+      <div className="container">
+        <form>
+          <h1>List a House</h1>
+          <label>Short Title</label>
+          <input className="form-control" type="text" />
+          <label>Description</label>
+          <textarea
+            className="form-control"
+            placeholder="Describe your property.."
+            id="floatingTextarea2"
+            style={{ height: '100px' }}
+          ></textarea>
+          <label>Number of Rooms</label>
+          <input className="form-control" type="number" />
+          <label>Location</label>
+          <select className="form-control">
+            <option>Koh Samui</option>
+            <option>Koh Phangan</option>
+            <option>Mars</option>
+          </select>
+          <label>Price (per night)</label>
+          <input className="form-control" type="number" />
+          <label>Add Photos(up to 9)</label>
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <input
+            className="form-control mb-2"
+            placeholder="http://.."
+            type="text"
+          />
+          <button
+            className="form-control btn btn-success mb-5"
+            type="submit"
+            style={{ width: '100px' }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default HouseCreate
