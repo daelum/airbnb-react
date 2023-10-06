13e9efdeaf848d2bc69828946b16f28e88804616
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-06 14:26:33 +0700

    removed header

--- src/pages/House.js
+++ src/pages/House.js
@@ -129,53 +129,7 @@ export default function House() {
   return (
     <>
       <Header />
-      {/* <!-- LOGGED OUT --> */}
-      {/* <!--<div className="header">
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <img className="logo" src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png">
-                    <form className="d-flex" role="search">
-                        <button className="btn btn-outline-success login-button me-2" type="submit">Login</button>
-                        <button className="btn btn-outline-success logout-button" type="submit">Logout</button>
-                    </form>
-                </div>
-            </nav>
-        </div> --> */}
-      {/* <!-- LOGGED IN --> */}
-      {/* <div>
-        <div className="header">
-          <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-              <img
-                alt=""
-                className="logo"
-                style={{ width: '7rem', height: 'auto' }}
-                src="https://res.cloudinary.com/dsko6ntfj/image/upload/v1642399114/portal/web%20development%20beginners/05%20Project%20Airbnb/assets/logo-airbnb.png"
-              />
-              <form className="d-flex" role="search">
-                <button
-                  className="btn btn-outline-success login-button me-2"
-                  type="submit"
-                >
-                  <img
-                    alt=""
-                    className="profile-pic rounded-circle me-1"
-                    style={{ width: '1.27rem', height: '1.27rem' }}
-                    src="images/IMG_8468.jpg"
-                  />
-                  <span>{house.host && house.host.name}</span>
-                </button>
-                <button
-                  className="btn btn-outline-success logout-button"
-                  type="submit"
-                >
-                  Logout
-                </button>
-              </form>
-            </div>
-          </nav>
-        </div>
-      </div>
+
       {/* <!-- main images --> */}
       <div className="container border-bottom border-top">
         <div className="row">
